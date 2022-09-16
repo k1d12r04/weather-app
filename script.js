810a9ce9b8b24780998d5c5e6ff81f2a155beff3
Kadir <k1d12r04@gmail.com>
2022-09-16 18:56:43 +0300

    Revert "Search with click and enter"
    This reverts commit dda3649bfd20279b217c3d52f9a8aeb6d34267fd.

--- script.js
+++ script.js
@@ -4,24 +4,21 @@ const key = "be84b9dd7eeb3ac848f9a856d44146d0";
 const searchBar = document.getElementById("searchBar");
 const searchBtn = document.getElementById("searchBtn");
 
-searchBtn.addEventListener("click", (e) => {
-  getResult(searchBar.value);
-});
 
-searchBar.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    getResult(searchBar.value);
-  }
-});
+const setQuery = (e) => {
+  getResult(searchBar.value);
+}
+
+searchBtn.addEventListener("click", setQuery);
 
 const getResult = (cityName) => {
   let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
   fetch(query)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResult);
-};
+  .then(weather => {
+    return weather.json();
+  })
+  .then(displayResult)
+}
 
 const displayResult = (result) => {
   console.log(result);
@@ -32,7 +29,7 @@ const displayResult = (result) => {
   let event = document.querySelector(".weather-app__event");
   event.textContent = `${result.weather[0].description}`;
   let minmax = document.querySelector(".weather-app__minmax");
-  minmax.textContent = `${Math.round(result.main.temp_min)}°C / ${Math.round(
-    result.main.temp_max
-  )}°C`;
-};
+  minmax.textContent = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`;
+}
+
+
